7cd3ccf956a09314e79b4a6e57289fa4f501da24
JyotiGhising1 <83853614+JyotiGhising1@users.noreply.github.com>
2023-12-19 15:18:51 -0500

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name, eventName){
+     const emptyArray =[];
+    for(let i=0; i<name.length; i++){
+       const message= `Thank you, ${name[i]}, for the wonderful ${eventName} gift!`;
+       emptyArray.push(message);
+    }
+    return emptyArray;
+}
+
+function countDown(positiveInterger){
+    let number = positiveInterger;
+    while(number>=0){
+        console.log(number);
+        number--;
+    } 
+}
